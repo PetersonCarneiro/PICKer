9cac1502fa938021e95b76dee113e7e3388a468e
Peterson <peters.prestes@gmail.com>
2024-07-29 22:27:27 -0300

    Feat: create code for the barcode

---
+++ app/src/main/java/com/br/picker/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.br.picker;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/br/picker/InputActivity.java
+++ app/src/main/java/com/br/picker/InputActivity.java
@@ -1,10 +1,16 @@
 package com.br.picker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -15,8 +21,26 @@ public class InputActivity extends AppCompatActivity {
     }
 
     public void btnScan(View view){
-        Intent intent = new Intent();
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volumn up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+
     }
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result ->{
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
 
     public void btnEnter(View view){
         Intent intent = new Intent(this, RecyclerViewItem.class);
